6beb7e6478543e2dd4009af5099c0cf19119105a
devplayg <devplayg@gmail.com>
2019-05-14 20:21:12 +0900

    test changing user role dynamically

--- src/main/java/com/devplayg/vms/config/SecurityConfig.java
+++ src/main/java/com/devplayg/vms/config/SecurityConfig.java
@@ -2,23 +2,24 @@ package com.devplayg.vms.config;
 
 import com.devplayg.vms.service.MemberDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true,
-        jsr250Enabled = true
-)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -26,17 +27,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        auth.userDetailsService(memberDetailsService).passwordEncoder(encoder);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        auth
+            .userDetailsService(memberDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-//        log.debug("### Security configure");
-        http.csrf().disable().cors();
 
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        log.debug("### Security configure");
+//        httpSecurity.csrf().disable().cors();
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        http
+        httpSecurity
             .authorizeRequests()
 
                 // 아래 URL 패턴에 매칭되면
@@ -94,10 +98,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
             .addFilterBefore(filter, CsrfFilter.class)
-                .csrf().disable();
-//            .build();
+                .csrf().disable()
+
+            .sessionManagement().maximumSessions(10).sessionRegistry(sessionRegistry());
+//            .build(); .sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
 
     }
+
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+
+//    @Bean
+//    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+//        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+//    }
+
 
 
 //    @Bean
--- src/main/java/com/devplayg/vms/controller/SecurityController.java
+++ src/main/java/com/devplayg/vms/controller/SecurityController.java
@@ -1,20 +1,37 @@
 package com.devplayg.vms.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
-//@RequestMapping("sec")
 public class SecurityController {
 
-//    @Autowired
-//    private SecurityService securityService;
+    @Autowired
+    private SessionRegistry sessionRegistry;
 
     @GetMapping("/sec/read")
     @PreAuthorize("hasAnyRole('ADMIN','SHERIFF', 'USER')")
@@ -39,4 +56,169 @@ public class SecurityController {
         log.info("in user:    {}", req.isUserInRole("ROLE_USER"));
         return "write";
     }
+
+    // 자신에게 권한 추가
+    @GetMapping("addRole/{role}")
+    public String active(@PathVariable("role") String role) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        List<GrantedAuthority> roles = new ArrayList<>(auth.getAuthorities());
+        roles.add( new SimpleGrantedAuthority("ROLE_"+role.toUpperCase()));
+        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), roles);
+        SecurityContextHolder.getContext().setAuthentication(newAuth);
+        return "";
+    }
+
+    // 사용자에게 권한 추가
+    @GetMapping("assign/rule/{username}/{role}")
+    public String assignRole(@PathVariable("username") String username, @PathVariable("role") String role) {
+//        loadUserDetails
+//        final List<Object> list = sessionRegistry.getAllPrincipals();
+//        for ( Object abc : list) {
+//                UserDetails nn = (UserDetails) abc;
+//        }
+
+        List<UserDetails> list = sessionRegistry.getAllPrincipals()
+                .stream()
+                .map(e -> (UserDetails) e)
+                .collect(Collectors.toList());
+
+        for (UserDetails user : list) {
+            if (user.getUsername().equals(username)) {
+
+                List<GrantedAuthority> roles = new ArrayList<>(user.getAuthorities());
+                roles.add( new SimpleGrantedAuthority("ROLE_"+role.toUpperCase()));
+                Authentication newAuth = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), roles);
+
+//                UserDetailsByNameServiceWrapper loadUserDetails
+
+//                SecurityContextHolder.getContext().setAuthentication(newAuth);
+                List<SessionInformation> sessions = sessionRegistry.getAllSessions(user, false);
+                for (SessionInformation s : sessions) {
+//                    s.getSessionId()
+//
+                }
+//                getSession
+//                HttpSession session = request.getSession();
+//                session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+//                log.info("session info: {}", sessions.toString());
+
+//                user.
+//                Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), roles);
+//                user.getAuthorities().add(new SimpleGrantedAuthority("ROLE_"));
+//                grantedAuths.add();
+
+//                user.getAuthorities().add();
+
+
+                log.info("username: {}, details: {}",  user.getUsername(), user);
+            }
+        }
+
+//        SecurityContextHolder.
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        List<GrantedAuthority> roles = new ArrayList<>(auth.getAuthorities());
+//        roles.add( new SimpleGrantedAuthority("ROLE_"+role.toUpperCase()));
+//        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), roles);
+//        SecurityContextHolder.getContext().setAuthentication(newAuth);
+        return "";
+    }
+
+    @GetMapping("readRole")
+    public ResponseEntity<?> readRole() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        List<GrantedAuthority> roles = new ArrayList<>(auth.getAuthorities());
+        return new ResponseEntity<>(roles, HttpStatus.OK);
+    }
+
+    @GetMapping("loggedUsers")
+    public ResponseEntity<?> getLoggedUser() {
+        List<Object> loggedUsers = sessionRegistry.getAllPrincipals();
+        log.info("logged: {}", loggedUsers);
+        return new ResponseEntity<>(loggedUsers, HttpStatus.OK);
+    }
+
+//    @GetMapping("loggedUsers2")
+//    public ResponseEntity<?> readRole(@PathVariable("username") String username) {
+//        final List<Object> loggedUsers = sessionRegistry.getAllPrincipals();
+//        log.info("logged: {}", loggedUsers);
+//        return new ResponseEntity<>(loggedUsers, HttpStatus.OK);
+//        List<Object> loggedUsers = sessionRegistry.getAllPrincipals();
+//        return new ResponseEntity<>(loggedUsers, HttpStatus.OK);
+//        SecurityContextHolder.getContext().getAuthentication()
+
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        List<GrantedAuthority> roles = new ArrayList<>(auth.getAuthorities());
+//        return new ResponseEntity<>(roles, HttpStatus.OK);
+//    }
+//        updatedAuthorities.add(...); //add your role here [e.g., new SimpleGrantedAuthority("ROLE_NEW_ROLE")]
+//
+//
+//        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), updatedAuthorities);
+//
+//        SecurityContextHolder.getContext().setAuthentication(newAuth);
+
+//        List<Object> loggedUsers = sessionRegistry.getAllPrincipals();
+
+
+        // user object = User currently updated
+// invalidate user session
+        // https://stackoverflow.com/questions/9910252/how-to-reload-authorities-on-user-update-with-spring-security
+//        List<Object> loggedUsers = sessionRegistry.getAllPrincipals();
+//        for (Object principal : loggedUsers) {
+//            if(principal instanceof User) {
+//                final User loggedUser = (User) principal;
+//                if(user.getUsername().equals(loggedUser.getUsername())) {
+//                    List<SessionInformation> sessionsInfo = sessionRegistry.getAllSessions(principal, false);
+//                    if(null != sessionsInfo && sessionsInfo.size() > 0) {
+//                        for (SessionInformation sessionInformation : sessionsInfo) {
+//                            LOGGER.info("Exprire now :" + sessionInformation.getSessionId());
+//                            sessionInformation.expireNow();
+//                            sessionRegistry.removeSessionInformation(sessionInformation.getSessionId());
+//                            // User is not forced to re-logging
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        inMemoryUserDetailsManager.
+//        return "";
+//    }
+
+
+//  [
+//    {
+//            "username":"jisan",
+//            "authorities":[
+//        {
+//            "authority":"ROLE_ADMIN"
+//        },
+//        {
+//            "authority":"ROLE_SHERIFF"
+//        },
+//        {
+//            "authority":"ROLE_USER"
+//        }
+//      ],
+//        "accountNonExpired":true,
+//            "accountNonLocked":true,
+//            "credentialsNonExpired":true,
+//            "enabled":true
+//    },
+//    {
+//            "username":"wsahn",
+//            "authorities":[
+//        {
+//            "authority":"ROLE_ADMIN"
+//        },
+//        {
+//            "authority":"ROLE_USER"
+//        }
+//      ],
+//        "accountNonExpired":true,
+//            "accountNonLocked":true,
+//            "credentialsNonExpired":true,
+//            "enabled":true
+//    }
+//]
 }
